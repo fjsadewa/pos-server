eb0eefde76ebd4d849fd0d727e81c54e54c109d5
fjsadewa <121071655+fjsadewa@users.noreply.github.com>
2024-06-05 13:58:33 +0700

    update-function-print

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
-const ThermalPrinter = require('node-thermal-printer');
+const fs = require('fs');
+const { ThermalPrinter }  = require('node-thermal-printer');
+//const baseUrl = "http://localhost/laravel-10/antrianpos-app/public";
+
+//const printer = new ThermalPrinter({
+//  width: 80,
+//  interface: "usb", // Ganti dengan interface yang sesuai (usb, serial, dll)
+//  port: "/dev/usb0", // Ganti dengan port yang sesuai
+//  model: "BP-T80P", // Model printer Blueprint BP-T80P
+//});
+
+
+ const printer = new ThermalPrinter({
+   width: 80,
+ });
 
 const app = express();
 const server = createServer(app);
@@ -10,7 +24,16 @@ const io = new Server(server);
 
 app.get('/', async (req, res) => {
       res.sendFile(join(__dirname, 'index.html'));
-  });
+});
+
+async function getQueueData() {
+  const response = await fetch('http://localhost/laravel-10/antrianpos-app/public/getAntrian');
+  const data = await response.json();
+}
+
+app.get('/print', async (req, res) => {
+  res.sendFile(join(__dirname, 'print.html'));
+});
 
 app.get('/display', async (req, res) => {
   res.sendFile(join(__dirname, 'display.html'));
@@ -30,23 +53,40 @@ app.get('/call', (req, res) => {
   res.send('calling');
 });
 
-app.post('/print', async (req, res) => {
-  const data = req.body.data; // Dapatkan data cetak dari request body
-  const printer = new ThermalPrinter({
-    width: 80,
-    characterSet: 'A'
-  });
+//app.get("/print-queue", async (req, res) => {
+//  try {
+//    await printer.connect();
+//
+//    const response = await fetch(`${baseUrl}/getAntrian`);
+//    const queueData = await response.json();
+//
+//    if (queueData) {
+//      printer.align("center");
+//      printer.print("Pos Indonesia");
+//      printer.feed(2);
+//
+//      printer.align("left");
+//      printer.print(`Nomor Antrian: ${queueData.nomor_urut}`);
+//      printer.print(`Kode Pelayanan: ${queueData.kode_pelayanan}`);
+//      printer.print(`Nama Pelayanan: ${queueData.nama_pelayanan}`);
+//      printer.print(`Waktu Diambil: ${queueData.timestamp}`);
+//      printer.feed(2);
+//
+//      printer.cut();
+//      await printer.disconnect();
+//    } else {
+//      await printer.disconnect();
+//    }
+//
+//    res.status(200).json({ message: "Data antrian telah dicetak" });
+//  } catch (error) {
+//    console.error("Error:", error);
+//    res.status(500).json({ error: error.message });
+//  }
+//});
 
-  try {
-    await printer.print(data); // Cetak data ke printer
-    res.status(200).send('Data dicetak dengan sukses!');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Gagal mencetak data!');
-  } finally {
-    await printer.close(); // Tutup koneksi dengan printer
-  }
-});
+const templateHTML = fs.readFileSync('print.html', 'utf8');
+printer.print(templateHTML);
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -58,7 +98,12 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
-
-
-
-// const parseRequest = require('parse-request');
+// term & conditions di notifikasi
+// searching destination
+// searching package
+// searching whislist *perlu atau tidak
+// see all offer ini kemana
+// search page
+// header detail destination
+// milih tanggal di booking page
+// dropdown di package 
