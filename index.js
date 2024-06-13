f857b0fb441332d8771b05f2b5ea7e50de906493
fjsadewa <121071655+fjsadewa@users.noreply.github.com>
2024-06-13 19:42:52 +0700

    update-display

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ const { join } = require('node:path');
 const { Server } = require('socket.io');
 const fs = require('fs');
 const { ThermalPrinter }  = require('node-thermal-printer');
-//const baseUrl = "http://localhost/laravel-10/antrianpos-app/public";
-
-//const printer = new ThermalPrinter({
-//  width: 80,
-//  interface: "usb", // Ganti dengan interface yang sesuai (usb, serial, dll)
-//  port: "/dev/usb0", // Ganti dengan port yang sesuai
-//  model: "BP-T80P", // Model printer Blueprint BP-T80P
-//});
-
 
  const printer = new ThermalPrinter({
    width: 80,
@@ -26,10 +17,10 @@ app.get('/', async (req, res) => {
       res.sendFile(join(__dirname, 'index.html'));
 });
 
-async function getQueueData() {
-  const response = await fetch('http://localhost/laravel-10/antrianpos-app/public/getAntrian');
-  const data = await response.json();
-}
+// async function getQueueData() {
+//   const response = await fetch('http://localhost/laravel-10/antrianpos-app/public/getAntrian');
+//   const data = await response.json();
+// }
 
 app.get('/print', async (req, res) => {
   res.sendFile(join(__dirname, 'print.html'));
@@ -52,38 +43,6 @@ app.get('/call', (req, res) => {
   io.emit('call', req.query.sequence);
   res.send('calling');
 });
-
-//app.get("/print-queue", async (req, res) => {
-//  try {
-//    await printer.connect();
-//
-//    const response = await fetch(`${baseUrl}/getAntrian`);
-//    const queueData = await response.json();
-//
-//    if (queueData) {
-//      printer.align("center");
-//      printer.print("Pos Indonesia");
-//      printer.feed(2);
-//
-//      printer.align("left");
-//      printer.print(`Nomor Antrian: ${queueData.nomor_urut}`);
-//      printer.print(`Kode Pelayanan: ${queueData.kode_pelayanan}`);
-//      printer.print(`Nama Pelayanan: ${queueData.nama_pelayanan}`);
-//      printer.print(`Waktu Diambil: ${queueData.timestamp}`);
-//      printer.feed(2);
-//
-//      printer.cut();
-//      await printer.disconnect();
-//    } else {
-//      await printer.disconnect();
-//    }
-//
-//    res.status(200).json({ message: "Data antrian telah dicetak" });
-//  } catch (error) {
-//    console.error("Error:", error);
-//    res.status(500).json({ error: error.message });
-//  }
-//});
 
 const templateHTML = fs.readFileSync('print.html', 'utf8');
 printer.print(templateHTML);
