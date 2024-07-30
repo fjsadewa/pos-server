b107fe6c5037ad90133649ffe76b6db255a3dc52
fjsadewa <121071655+fjsadewa@users.noreply.github.com>
2024-07-30 20:58:05 +0700

    update-auto-ip

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
+require('dotenv').config();
+const mysql = require('mysql');
+
 const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
 const fs = require('fs');
 const { ThermalPrinter }  = require('node-thermal-printer');
+
+const readEnvFile = () => {
+  if (fs.existsSync('.env')) {
+    const envData = fs.readFileSync('.env', 'utf8');
+    return envData.split('\n').reduce((acc, line) => {
+      const [key, value] = line.split('=');
+      if (key && value !== undefined) {
+        acc[key.trim()] = value.trim();
+      }
+      return acc;
+    }, {});
+  }
+  return {};
+};
+
+const dbConfig = {
+  host: process.env.DB_HOST || '127.0.0.1',
+  user: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_DATABASE || 'antrianpos'
+};
+
+const connection = mysql.createConnection(dbConfig);
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to database as id ' + connection.threadId);
+});
 
 const printer = new ThermalPrinter({
   width: 80,
@@ -14,13 +48,12 @@ const server = createServer(app);
 const io = new Server(server);
 
 app.get('/', async (req, res) => {
-      res.sendFile(join(__dirname, 'index.html'));
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
-// async function getQueueData() {
-//   const response = await fetch('http://localhost/laravel-10/antrianpos-app/public/getAntrian');
-//   const data = await response.json();
-// }
+app.get('/env', (req, res) => {
+  res.json({ NOMOR_IP: process.env.NOMOR_IP });
+});
 
 app.get('/print', async (req, res) => {
   res.sendFile(join(__dirname, 'print.html'));
@@ -49,6 +82,42 @@ app.get('/call', (req, res) => {
   res.send('calling');
 });
 
+const writeEnvFile = (data) => {
+  const envData = Object.keys(data).map(key => `${key}=${data[key]}`).join('\n');
+  fs.writeFileSync('.env', envData, 'utf8');
+};
+
+const saveIpServerToEnv = () => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM ip_servers LIMIT 1', (err, results) => {
+      if (err) {
+        reject('Database query failed: ' + err.message);
+        return;
+      }
+      if (results.length === 0) {
+        reject('No data found in ip_servers table');
+        return;
+      }
+
+      const ipData = results[0];
+      const envData = readEnvFile();
+
+      if (envData.NOMOR_IP !== ipData.nomor_ip) {
+        envData.NOMOR_IP = ipData.nomor_ip;
+
+        writeEnvFile(envData);
+        resolve('Data saved to .env');
+      } else {
+        resolve('No changes in data');
+      }
+    });
+  });
+};
+
+saveIpServerToEnv()
+  .then(message => console.log(message))
+  .catch(error => console.error(error));
+
 const templateHTML = fs.readFileSync('print.html', 'utf8');
 printer.print(templateHTML);
 
@@ -60,14 +129,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(3000, () => {
-  console.log('server running at http://192.168.5.160:3000');
+  console.log('server running');
 });
-// term & conditions di notifikasi
-// searching destination
-// searching package
-// searching whislist *perlu atau tidak
-// see all offer ini kemana
-// search page
-// header detail destination
-// milih tanggal di booking page
-// dropdown di package 
